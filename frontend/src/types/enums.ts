3438fdca332d79b53a85d9f9e73e208bf8df159c
Xuan Pham <hpham024@uottawa.ca>
2026-03-12 19:40:01 -0400

    create types for literal constants

---
+++ frontend/src/types/enums.ts
@@ -0,0 +1,3 @@
+export type Amenity = "WiFi" | "AC" | "Heating" | "TV" | "Fridge" | "Closet" | "Balcony" | "Desk" | "Couch" | "Microwave" | "Safe" | "Pool" | "Gym" | "Parking";
+export type View = "City" | "Ocean" | "Mountain";
+export type RoomType = "Single" | "Double" | "Suite";
