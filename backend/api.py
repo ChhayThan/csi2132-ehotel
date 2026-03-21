c000093a7340f3184946bf3275548b5b2d95ed4e
Xuan Pham <hpham024@uottawa.ca>
2026-03-21 14:16:42 -0400

    api skeleton using auth token

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,7 @@
 # api to query the database server
 
 from datetime import date
+from typing_extensions import Literal
 
 from fastapi import FastAPI
 from psycopg2 import connect
@@ -51,27 +52,45 @@ def get_room_details(hotel_id: int, room_number: int) -> Room:
     pass
 
 
+@app.post("/auth/login")
+def login(email: str, password: str) -> str:
+    """Returns token"""
+    pass
+
+
+@app.post("/auth/register")
+def register(email: str, password: str, first_name: str, last_name: str, drivers_license: str) -> str:
+    """Returns token"""
+    pass
+
+
 ## Customer APIs - require authentication
 
 @app.get("/{customer_id}/bookings")
-def get_bookings(customer_id: int, archived: bool, username: str, password: str) -> list[Booking]:
+def get_bookings(customer_id: int, archived: bool, token: str) -> list[Booking]:
     pass
 
 
 @app.get("/{customer_id}/bookings/{booking_id}")
-def get_booking_details(customer_id: int, booking_id: int, username: str, password: str) -> Booking:
+def get_booking_details(customer_id: int, booking_id: int, token: str) -> Booking:
     pass
 
 
 ## Employee APIs - require authentication
 
 @app.get("/employee/hotels/{hotel_id}/bookings")
-def get_hotel_bookings(hotel_id: int, archived: bool, username: str, password: str) -> list[Booking]:
+def get_hotel_bookings(hotel_id: int, archived: bool, token: str) -> list[Booking]:
     pass
 
 
 @app.get("/employee/hotels/{hotel_id}/rentings")
-def get_hotel_rentings(hotel_id: int, archived: bool, username: str, password: str) -> list[Renting]:
+def get_hotel_rentings(hotel_id: int, archived: bool, token: str) -> list[Renting]:
+    pass
+
+
+@app.post("/employee/login")
+def employee_login(employee_id: int, password: str) -> str:
+    """Returns token"""
     pass
 
 
@@ -81,16 +100,76 @@ def get_hotel_chains(username: str, password: str) -> list[HotelChain]:
     pass
 
 
+@app.post("/admin/hotel_chains/new")
+def create_hotel_chain(name: str, address: str, phone_number: str, email_addresses: list[str], token: str) -> str:
+    pass
+
+
+@app.put("/admin/hotel_chains/{chain_name}/manage")
+def edit_hotel_chain(chain_name: str, token: str, name: str = None, address: str = None, phone_number: str = None, email_addresses: list[str] = None) -> str:
+    pass
+
+
+@app.delete("/admin/hotel_chains/{chain_name}/delete")
+def delete_hotel_chain(chain_name: str, token: str):
+    pass
+
+
 @app.get("/admin/hotel_chains/{chain_name}/hotels")
-def get_hotels_in_chain(chain_name: str, username: str, password: str) -> list[Hotel]:
+def get_hotels_in_chain(chain_name: str, token: str) -> list[Hotel]:
+    pass
+
+
+@app.post("/admin/hotel_chains/{chain_name}/hotels/new")
+def create_hotel_in_chain(chain_name: str, name: str, rating: int, city: str, street_address: str, country: str, image: str, phone_number: str, email_addresses: list[str], token: str) -> int:
+    pass
+
+
+@app.put("/admin/hotel_chains/{chain_name}/hotels/{hotel_id}/manage")
+def edit_hotel_in_chain(chain_name: str, hotel_id: int, token: str, name: str = None, rating: int = None, city: str = None, street_address: str = None, country: str = None, image: str = None, phone_number: str = None, email_addresses: list[str] = None) -> int:
+    pass
+
+
+@app.delete("/admin/hotel_chains/{chain_name}/hotels/{hotel_id}/delete")
+def delete_hotel_in_chain(chain_name: str, hotel_id: int, token: str):
     pass
 
 
 @app.get("/admin/hotel_chains/{chain_name}/hotels/{hotel_id}/rooms")
-def get_rooms_in_hotel(chain_name: str, hotel_id: int, username: str, password: str) -> list[Room]:
+def get_rooms_in_hotel(chain_name: str, hotel_id: int, token: str) -> list[Room]:
+    pass
+
+
+@app.post("/admin/hotel_chains/{chain_name}/hotels/{hotel_id}/rooms/new")
+def create_room_in_hotel(chain_name: str, hotel_id: int, room_number: int, capacity: Literal[1, 2, 3, 4], view: str, price: float, problem: str, extendable: bool, amenities: list[str], token: str) -> int:
+    pass
+
+
+@app.put("/admin/hotel_chains/{chain_name}/hotels/{hotel_id}/rooms/{room_number}/manage")
+def edit_room_in_hotel(chain_name: str, hotel_id: int, room_number: int, token: str, new_room_number: int = None, capacity: Literal[1, 2, 3, 4] = None, view: str = None, price: float = None, problem: str = None, extendable: bool = None, amenities: list[str] = None) -> int:
+    pass
+
+
+@app.delete("/admin/hotel_chains/{chain_name}/hotels/{hotel_id}/rooms/{room_number}/delete")
+def delete_room_in_hotel(chain_name: str, hotel_id: int, room_number: int, token: str):
     pass
 
 
 @app.get("/admin/hotel_chains/{chain_name}/hotels/{hotel_id}/employees")
-def get_employees_in_hotel(chain_name: str, hotel_id: int, username: str, password: str) -> list[Employee]:
+def get_employees_in_hotel(chain_name: str, hotel_id: int, token: str) -> list[Employee]:
+    pass
+
+
+@app.post("/admin/hotel_chains/{chain_name}/hotels/{hotel_id}/employees/new")
+def create_employee_in_hotel(chain_name: str, hotel_id: int, first_name: str, last_name: str, password: str, role: str, address: str, token: str) -> int:
+    pass
+
+
+@app.put("/admin/hotel_chains/{chain_name}/hotels/{hotel_id}/employees/{employee_id}/manage")
+def edit_employee_in_hotel(chain_name: str, hotel_id: int, employee_id: int, token: str, first_name: str = None, last_name: str = None, password: str = None, role: str = None, address: str = None) -> int:
+    pass
+
+
+@app.delete("/admin/hotel_chains/{chain_name}/hotels/{hotel_id}/employees/{employee_id}/delete")
+def delete_employee_in_hotel(chain_name: str, hotel_id: int, employee_id: int, token: str):
     pass
