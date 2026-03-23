a64eafb05015c2fa52c9a7842cab0eda2ef76846
atallaL <leonardoyatalla@gmail.com>
2026-03-23 16:58:35 -0400

    added global navbar

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,19 +1,29 @@
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import ComponentPreviewPage from "./component_preview_page";
 import ConfirmBookingPage from "./confirm_booking_page";
 import EmployeeDashboardPage from "./employee_dashboard_page";
 import EmployeeLoginPage from "./employee_login_page";
 import Home from "./home";
+import Navbar from "../components/navbar/navbar.tsx"
 
 function App() {
+
+  const [userType, setUserType] = useState("Guest");
+  const [name, setName] = useState("John Doe");
+  const [currency, setCurrency] = useState("CAD");
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/employee/login" element={<EmployeeLoginPage />} />
-      <Route path="/employee/dashboard" element={<EmployeeDashboardPage />} />
-      <Route path="/preview/components" element={<ComponentPreviewPage />} />
-      <Route path="/preview/confirm-booking" element={<ConfirmBookingPage />} />
-    </Routes>
+    <div className="flex flex-col pt-18">
+      <Navbar user_type={userType} user_name={name} currency={currency} setCurrency={setCurrency}/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/employee/login" element={<EmployeeLoginPage />} />
+        <Route path="/employee/dashboard" element={<EmployeeDashboardPage />} />
+        <Route path="/preview/components" element={<ComponentPreviewPage />} />
+        <Route path="/preview/confirm-booking" element={<ConfirmBookingPage />} />
+      </Routes>
+    </div>
   );
 }
 
